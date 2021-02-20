68c5447c4f0bbb2574fe4810893ee67337ec1afe
Saidev Mohapatra <saimohap@microsoft.com>
2021-02-20 22:05:09 +0530

    added kendo dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {DataPicker, DatePicker} from '@progress/kendo-dateinputs-react-wrapper';
+import '@progress/kendo-ui';
+import '@progress/kendo-theme-default/dist/all.css'
+import { Component } from 'react';
 
-function App() {
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      dateTime: new Date()
+    }
+    this.onchange = this.onchange.bind(this);
+  }
+  
+  onchange(e) {
+    console.log(e.sender.value());
+    this.setState({
+      dateTime: e.sender.value()
+    });
+  };
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="date-pocker">
+        <DatePicker
+        value={this.dateTime}
+        min = {new Date(2020,11,1)}
+        max = {new Date(2021,11,1)}
+        format = {"'yyyy/MM/dd"}
+        change = {this.onchange}
+        />
+      </div>
     </div>
   );
+}
 }
 
 export default App;
